340383e0d9dfd7bc7b74f4246d176edd5fe3e4f9
slavakazak <slavakazak3@gmail.com>
2023-05-18 00:52:43 +0300

    Favorites

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,21 +1,38 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { Job } from './Interfaces/Jobs'
 
 interface JobCard {
 	job: Job
-	isFavorite?: boolean
 	single?: boolean
 }
 
-export function JobCard({ job, isFavorite = false, single = false }: JobCard) {
-	let salary = ''
+export function JobCard({ job, single = false }: JobCard) {
+	const [favorite, setFavorite] = useState(false)
 
+	let salary = ''
 	if (job.payment_from !== 0 && job.payment_to !== 0) {
 		salary = 'з/п ' + job.payment_from + ' - ' + job.payment_to + ' ' + job.currency
 	} else if (job.payment_from !== 0 && job.payment_to === 0) {
 		salary = 'з/п от ' + job.payment_from + ' ' + job.currency
 	} else if (job.payment_from === 0 && job.payment_to !== 0) {
 		salary = 'з/п ' + job.payment_to + ' ' + job.currency
+	}
+
+	useEffect(() => {
+		setFavorite(JSON.parse(localStorage.getItem('favorites') || '[]').includes(job.id))
+	}, [])
+
+	function favoriteClickHandler() {
+		let favorites = JSON.parse(localStorage.getItem('favorites') || '[]')
+		let isFavorite = favorites.includes(job.id)
+		if (isFavorite) {
+			favorites = favorites.filter((item: number) => item !== job.id)
+		} else {
+			favorites.push(job.id)
+		}
+		localStorage.setItem('favorites', JSON.stringify(favorites))
+		setFavorite(!isFavorite)
 	}
 
 	return (
@@ -28,7 +45,11 @@ export function JobCard({ job, isFavorite = false, single = false }: JobCard) {
 						<h2>{job.profession}</h2>
 					</Link>
 				)}
-				<svg viewBox='0 0 24 24' fill='none' className={'favorite' + (isFavorite ? ' active' : '')}>
+				<svg
+					viewBox='0 0 24 24'
+					fill='none'
+					className={'favorite' + (favorite ? ' active' : '')}
+					onClick={favoriteClickHandler}>
 					<path
 						d='M10.9718 2.70846C11.4382 1.93348 12.5618 1.93348 13.0282 2.70847L15.3586 6.58087C15.5262 6.85928 15.7995 7.05784 16.116 7.13116L20.5191 8.15091C21.4002 8.35499 21.7474 9.42356 21.1545 10.1066L18.1918 13.5196C17.9788 13.765 17.8744 14.0863 17.9025 14.41L18.2932 18.9127C18.3714 19.8138 17.4625 20.4742 16.6296 20.1214L12.4681 18.3583C12.1689 18.2316 11.8311 18.2316 11.5319 18.3583L7.37038 20.1214C6.53754 20.4742 5.62856 19.8138 5.70677 18.9127L6.09754 14.41C6.12563 14.0863 6.02124 13.765 5.80823 13.5196L2.8455 10.1066C2.25257 9.42356 2.59977 8.35499 3.48095 8.15091L7.88397 7.13116C8.20053 7.05784 8.47383 6.85928 8.64138 6.58087L10.9718 2.70846Z'
 						strokeWidth='1.5'
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,49 +1,57 @@
-import { Vacancies } from '@/components/Interfaces/Jobs'
+import { Vacancies, Job } from '@/components/Interfaces/Jobs'
 import { MainLayout } from '@/components/MainLayout'
 import { JobCard } from '@/components/JobCard'
 import { Pagination } from '@/components/Pagination'
 import { getFetchParams, getJSON, FetchParams } from '@/utils/startupSummerAPI'
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
-export default function Favorites({ vacancies }: { vacancies: Vacancies }) {
+export default function Favorites() {
+	const router = useRouter()
+	const [vacancies, setVacancies] = useState<Vacancies | undefined>()
+	useEffect(() => {
+		async function load() {
+			const { page } = router.query
+			const fetchParams: FetchParams = await getFetchParams()
+			const favorites = JSON.parse(localStorage.getItem('favorites') || '[]')
+			const favoritesQuery = favorites.map((favorite: number) => 'ids[]=' + favorite).join('&')
+			const vacanciesJSON: Vacancies = await getJSON(
+				'/2.0/vacancies/?' + favoritesQuery,
+				{
+					count: '4',
+					page: String(page && +page - 1),
+				},
+				fetchParams
+			)
+			const total = vacanciesJSON.total > 500 ? 500 : vacanciesJSON.total
+			const maxPage = Math.ceil(total / 4)
+			if (
+				!vacanciesJSON ||
+				!vacanciesJSON.objects ||
+				vacanciesJSON.objects.length === 0 ||
+				favorites.length === 0 ||
+				(page && (+page > maxPage || +page < 1))
+			) {
+				router.push('/404')
+			} else {
+				setVacancies(vacanciesJSON)
+			}
+		}
+
+		load()
+	}, [router])
+
 	return (
 		<MainLayout title='Jobored | Избранное' description='Страница избранного'>
-			<main>
-				{vacancies.objects.map(job => (
-					<JobCard key={job.id} job={job} />
-				))}
-				<Pagination total={vacancies.total > 500 ? 500 : vacancies.total} />
-			</main>
+			{vacancies && (
+				<main>
+					{vacancies.objects.map(job => (
+						<JobCard key={job.id} job={job} />
+					))}
+					<Pagination total={vacancies.total > 500 ? 500 : vacancies.total} />
+				</main>
+			)}
 		</MainLayout>
 	)
-}
-
-export const getServerSideProps: GetServerSideProps<{ vacancies: Vacancies }> = async context => {
-	const { page } = context.query
-	const fetchParams: FetchParams = await getFetchParams()
-	const vacanciesJSON: Vacancies = await getJSON(
-		'/2.0/vacancies/',
-		{
-			count: '4',
-			page: String(page && +page - 1),
-		},
-		fetchParams
-	)
-	const total = vacanciesJSON.total > 500 ? 500 : vacanciesJSON.total
-	const maxPage = Math.ceil(total / 4)
-	if (
-		!vacanciesJSON ||
-		!vacanciesJSON.objects ||
-		vacanciesJSON.objects.length === 0 ||
-		(page && (+page > maxPage || +page < 1))
-	) {
-		return {
-			notFound: true,
-		}
-	}
-	return {
-		props: {
-			vacancies: vacanciesJSON,
-		},
-	}
 }
